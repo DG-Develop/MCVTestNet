51eb5ba573ccfe70760d966b7350eeeaea041085
David Gómez <38304163+DG-Develop@users.noreply.github.com>
2021-06-26 17:31:53 -0500

    MVC project .Net Framework

---
+++ Controllers/CerrrarSesionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CursoMVC.Controllers
+{
+    public class CerrrarSesionController : Controller
+    {
+        // GET: CerrrarSesion
+        public ActionResult Logout()
+        {
+            Session["User"] = null;
+            return RedirectToAction("Index", "Access");
+        }
+    }
+}
